8f629590a38e2b1eb625288d393f0742b0573ec5
Alexandra Goldobina <goldobina.s@gmail.com>
2024-04-25 14:49:38 -0400

    Initial

---
+++ funcs.py
@@ -0,0 +1,10 @@
+def summation(a, b):
+    """
+    Суммирует 2 числа
+    :param a: первое число
+    :param b: второе число
+    :return: сумма двух чисел
+    """
+    sum_ = a + b
+    return sum_
+
